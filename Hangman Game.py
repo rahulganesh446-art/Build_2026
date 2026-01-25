a1e6e0e30a682e42d911b208fa2a5c316fd97e38
rahulganesh446-art <rahulganesh446@gmail.com>
2026-01-25 18:00:02 +0530

    Update Hangman Game.py

--- Hangman Game.py
+++ Hangman Game.py
@@ -59,7 +59,7 @@ stages = ['''
 ''']
 
 end_of_game = False
-word_list = ["ardvark", "baboon", "camel"]
+word_list = ["Athira", "Vishnu", "Rahul"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
